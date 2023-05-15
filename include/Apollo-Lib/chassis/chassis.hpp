b418841defedf19464a975e45b62b7cf9651a681
nikgajjar51 <nikgajjar51@gmail.com>
2023-05-14 20:59:54 -0400

    Restarting chassis class

--- include/Apollo-Lib/chassis/chassis.hpp
+++ include/Apollo-Lib/chassis/chassis.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "api.h"
-#include "chassis.hpp"
 namespace apollo {
 class Chassis {
   enum joystick_split_mode { SINGLE = 0, SPLIT = 1 };
@@ -8,14 +7,13 @@ class Chassis {
   int joystick_threshold;
   pros::motor_brake_mode_e_t drive_current_brake_mode =
       pros::E_MOTOR_BRAKE_COAST;
-  joystick_swing_mode drive_current_swing_mode;
-  pros::ADIEncoder *left_encoder_tracking_wheel;
-  pros::ADIEncoder *right_encoder_tracking_wheel;
-  pros::ADIEncoder *center_encoder_tracking_wheel;
-  pros::Rotation *left_rotation_tracking_wheel;
-  pros::Rotation *right_rotation_tracking_wheel;
-  pros::Rotation *center_rotation_tracking_wheel;
-  pros::Imu *inertial_sensor;
+  pros::ADIEncoder left_encoder_tracking_wheel;
+  pros::ADIEncoder right_encoder_tracking_wheel;
+  pros::ADIEncoder center_encoder_tracking_wheel;
+  pros::Rotation left_rotation_tracking_wheel;
+  pros::Rotation right_rotation_tracking_wheel;
+  pros::Rotation center_rotation_tracking_wheel;
+  pros::Imu inertial_sensor;
   std::vector<pros::Motor> left_tank_drive_motors;
   std::vector<pros::Motor> right_tank_drive_motors;
   /*
@@ -39,81 +37,56 @@ class Chassis {
    */
   Chassis(std::vector<int> left_drivetrain_motor_ports,
           std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
-          double chassis_gear_ratio);
-  Chassis(std::vector<int> left_drivetrain_motor_ports,
-          std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
           double chassis_gear_ratio, int inertial_sensor_port);
   Chassis(std::vector<int> left_drivetrain_motor_ports,
           std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
           double chassis_gear_ratio, int inertial_sensor_port,
           std::vector<int> left_encoder_tracking_wheel_ports,
           std::vector<int> right_encoder_tracking_wheel_ports,
-          double tracker_gear_ratio, double tracker_wheel_diameter);
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter);
   Chassis(std::vector<int> left_drivetrain_motor_ports,
           std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
-          double chassis_gear_ratio, int inertial_sensor_port,
-          std::vector<int> left_encoder_tracking_wheel_ports,
-          std::vector<int> right_encoder_tracking_wheel_ports,
-          std::vector<int> center_encoder_tracking_wheel_ports,
-          double tracker_gear_ratio, double tracker_wheel_diameter);
-  Chassis(std::vector<int> left_drivetrain_motor_ports,
-          std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
-          double chassis_gear_ratio, int inertial_sensor_port,
-          std::vector<int> left_encoder_tracking_wheel_ports,
-          std::vector<int> right_encoder_tracking_wheel_ports,
-          double tracker_gear_ratio, double tracker_wheel_diameter,
-          int expander_smart_port);
-  Chassis(std::vector<int> left_drivetrain_motor_ports,
-          std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
           double chassis_gear_ratio, int inertial_sensor_port,
           std::vector<int> left_encoder_tracking_wheel_ports,
           std::vector<int> right_encoder_tracking_wheel_ports,
           std::vector<int> center_encoder_tracking_wheel_ports,
-          double tracker_gear_ratio, double tracker_wheel_diameter,
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter);
+  Chassis(std::vector<int> left_drivetrain_motor_ports,
+          std::vector<int> right_drivetrain_motor_ports,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
+          double chassis_gear_ratio, int inertial_sensor_port,
+          std::vector<int> left_encoder_tracking_wheel_ports,
+          std::vector<int> right_encoder_tracking_wheel_ports,
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter,
           int expander_smart_port);
   Chassis(std::vector<int> left_drivetrain_motor_ports,
           std::vector<int> right_drivetrain_motor_ports,
-          int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
+          double chassis_gear_ratio, int inertial_sensor_port,
+          std::vector<int> left_encoder_tracking_wheel_ports,
+          std::vector<int> right_encoder_tracking_wheel_ports,
+          std::vector<int> center_encoder_tracking_wheel_ports,
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter,
+          int expander_smart_port);
+  Chassis(std::vector<int> left_drivetrain_motor_ports,
+          std::vector<int> right_drivetrain_motor_ports,
+          int chassis_cartidge_rpms, double chassis_wheel_diameter,
           double chassis_gear_ratio, int inertial_sensor_port,
           int left_rotation_tracking_wheel_ports,
-          int right_rotation_tracking_wheel_ports, double tracker_gear_ratio,
-          double tracker_wheel_diameter);
+          int right_rotation_tracking_wheel_ports,
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter);
   Chassis(std::vector<int> left_drivetrain_motor_ports,
           std::vector<int> right_drivetrain_motor_ports,
           int drivetrain_cartidge_rpms, double chassis_wheel_diameter,
           double chassis_gear_ratio, int inertial_sensor_port,
           int left_rotation_tracking_wheel_ports,
           int right_rotation_tracking_wheel_ports,
-          int center_rotation_tracking_wheel_ports, double tracker_gear_ratio,
-          double tracker_wheel_diameter);
-  void initialize();
-  void set_defaults();
-  void set_tank_control();
-  void set_arcade_standard_control();
-  void set_arcade_flipped_control();
-  void set_joystick_threshold(int threshold);
-  void set_chassis_brake_mode(pros::motor_brake_mode_e_t brake_mode);
-  void reset_drive_sensors();
-  void reset_inertial_sensor(int heading = 0);
-  double get_inertial_sensor_heading();
-  bool is_inertial_sensor_calibrating();
-  int left_sensor();
-  int left_velocity();
-  double left_mA();
-  bool is_left_drive_over_current();
-  int right_sensor();
-  int right_velocity();
-  double right_mA();
-  bool is_right_drive_over_current();
-  double calculate_tracker_tick_per_inch();
-  double calculate_tracker_tick_per_revoution();
-  double calculate_wheel_circumference(int diameter);
+          int center_rotation_tracking_wheel_ports,
+          double tracking_wheel_gear_ratio, double tracking_wheel_diameter);
+  void calculate_tracker_variables();
 
 private:
 #define tracker_motor_integrated 1;
--- src/Apollo-Lib/chassis/chassis.cpp
+++ src/Apollo-Lib/chassis/chassis.cpp
@@ -1,20 +1,18 @@
-#include "chassis/chassis.hpp"
 #include "main.h"
-#include "util.hpp"
 namespace apollo {
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
-                 double chassis_gear_ratio) {
-  left_encoder_tracking_wheel = nullptr;
-  right_encoder_tracking_wheel = nullptr;
-  center_encoder_tracking_wheel = nullptr;
-  left_rotation_tracking_wheel = nullptr;
-  right_rotation_tracking_wheel = nullptr;
-  center_rotation_tracking_wheel = nullptr;
-  inertial_sensor = nullptr;
+                 double chassis_gear_ratio, int inertial_sensor_port)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(-1, -1, -1),
+      right_encoder_tracking_wheel(-1, -1, -1),
+      center_encoder_tracking_wheel(-1, -1, -1),
+      left_rotation_tracking_wheel(-1, -1),
+      right_rotation_tracking_wheel(-1, -1),
+      center_rotation_tracking_wheel(-1, -1) {
   for (auto i : left_drivetrain_motor_ports) {
-    pros::Motor temp = pros::Motor(abs(i));
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
     left_tank_drive_motors.push_back(temp);
   }
   for (auto i : right_drivetrain_motor_ports) {
@@ -26,25 +24,52 @@ Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
   drivetrain_cartidge_rpms = chassis_cartidge_rpms;
   drivetrain_gear_ratio = chassis_gear_ratio;
   drivetrain_wheel_diameter = chassis_wheel_diameter;
-  drivetrain_wheel_circumference =
-      calculate_wheel_circumference(drivetrain_wheel_diameter);
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = drivetrain_gear_ratio;
   tracker_wheel_diameter = drivetrain_wheel_diameter;
   tracker_wheel_circumference = drivetrain_wheel_circumference;
-  tracker_gear_ratio = drivetrain_gear_ratio;
-  tracker_tick_per_inch = calculate_tracker_tick_per_inch();
-  tracker_tick_per_revolution = calculate_tracker_tick_per_revoution();
+  calculate_tracker_variables();
 }
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
-                 double chassis_gear_ratio, int inertial_sensor_port) {}
-Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
-                 std::vector<int> right_drivetrain_motor_ports,
-                 int chassis_cartidge_rpms, double chassis_wheel_diameter,
                  double chassis_gear_ratio, int inertial_sensor_port,
                  std::vector<int> left_encoder_tracking_wheel_ports,
                  std::vector<int> right_encoder_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter) {}
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(
+          abs(left_encoder_tracking_wheel_ports[0]),
+          abs(left_encoder_tracking_wheel_ports[1]),
+          util::is_negative(left_encoder_tracking_wheel_ports[0])),
+      right_encoder_tracking_wheel(
+          abs(right_encoder_tracking_wheel_ports[0]),
+          abs(right_encoder_tracking_wheel_ports[1]),
+          util::is_negative(right_encoder_tracking_wheel_ports[0])),
+      center_encoder_tracking_wheel(-1, -1, -1),
+      left_rotation_tracking_wheel(-1, -1),
+      right_rotation_tracking_wheel(-1, -1),
+      center_rotation_tracking_wheel(-1, -1) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
+  }
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
+  }
+  tracker_type = tracker_encoder_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
+}
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
@@ -52,15 +77,83 @@ Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> left_encoder_tracking_wheel_ports,
                  std::vector<int> right_encoder_tracking_wheel_ports,
                  std::vector<int> center_encoder_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter) {}
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(
+          abs(left_encoder_tracking_wheel_ports[0]),
+          abs(left_encoder_tracking_wheel_ports[1]),
+          util::is_negative(left_encoder_tracking_wheel_ports[0])),
+      right_encoder_tracking_wheel(
+          abs(right_encoder_tracking_wheel_ports[0]),
+          abs(right_encoder_tracking_wheel_ports[1]),
+          util::is_negative(right_encoder_tracking_wheel_ports[0])),
+      center_encoder_tracking_wheel(
+          abs(center_encoder_tracking_wheel_ports[0]),
+          abs(center_encoder_tracking_wheel_ports[1]),
+          util::is_negative(center_encoder_tracking_wheel_ports[0])),
+      left_rotation_tracking_wheel(-1, -1),
+      right_rotation_tracking_wheel(-1, -1),
+      center_rotation_tracking_wheel(-1, -1) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
+  }
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
+  }
+  tracker_type = tracker_encoder_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
+}
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
                  double chassis_gear_ratio, int inertial_sensor_port,
                  std::vector<int> left_encoder_tracking_wheel_ports,
                  std::vector<int> right_encoder_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter,
-                 int expander_smart_port) {}
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter, int expander_smart_port)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(
+          {expander_smart_port, abs(left_encoder_tracking_wheel_ports[0]),
+           abs(left_encoder_tracking_wheel_ports[1])},
+          util::is_negative(left_encoder_tracking_wheel_ports[0])),
+      right_encoder_tracking_wheel(
+          {expander_smart_port, abs(right_encoder_tracking_wheel_ports[0]),
+           abs(right_encoder_tracking_wheel_ports[1])},
+          util::is_negative(right_encoder_tracking_wheel_ports[0])),
+      center_encoder_tracking_wheel(-1, -1, -1),
+      left_rotation_tracking_wheel(-1, -1),
+      right_rotation_tracking_wheel(-1, -1),
+      center_rotation_tracking_wheel(-1, -1) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
+  }
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
+  }
+  tracker_type = tracker_encoder_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
+}
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
@@ -68,15 +161,82 @@ Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> left_encoder_tracking_wheel_ports,
                  std::vector<int> right_encoder_tracking_wheel_ports,
                  std::vector<int> center_encoder_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter,
-                 int expander_smart_port) {}
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter, int expander_smart_port)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(
+          {expander_smart_port, abs(left_encoder_tracking_wheel_ports[0]),
+           abs(left_encoder_tracking_wheel_ports[1])},
+          util::is_negative(left_encoder_tracking_wheel_ports[0])),
+      right_encoder_tracking_wheel(
+          {expander_smart_port, abs(right_encoder_tracking_wheel_ports[0]),
+           abs(right_encoder_tracking_wheel_ports[1])},
+          util::is_negative(right_encoder_tracking_wheel_ports[0])),
+      center_encoder_tracking_wheel(
+          {expander_smart_port, abs(center_encoder_tracking_wheel_ports[0]),
+           abs(center_encoder_tracking_wheel_ports[1])},
+          util::is_negative(center_encoder_tracking_wheel_ports[0])),
+
+      left_rotation_tracking_wheel(-1, -1),
+      right_rotation_tracking_wheel(-1, -1),
+      center_rotation_tracking_wheel(-1, -1) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
+  }
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
+  }
+  tracker_type = tracker_encoder_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
+}
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
                  double chassis_gear_ratio, int inertial_sensor_port,
                  int left_rotation_tracking_wheel_ports,
                  int right_rotation_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter) {}
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(-1, -1, -1),
+      right_encoder_tracking_wheel(-1, -1, -1),
+      center_encoder_tracking_wheel(-1, -1, -1),
+      left_rotation_tracking_wheel(
+          left_rotation_tracking_wheel_ports,
+          util::is_negative(left_rotation_tracking_wheel_ports)),
+      right_rotation_tracking_wheel(
+          right_rotation_tracking_wheel_ports,
+          util::is_negative(right_rotation_tracking_wheel_ports)),
+      center_rotation_tracking_wheel(-1, -1) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
+  }
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
+  }
+  tracker_type = tracker_rotation_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
+}
 Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  std::vector<int> right_drivetrain_motor_ports,
                  int chassis_cartidge_rpms, double chassis_wheel_diameter,
@@ -84,90 +244,48 @@ Chassis::Chassis(std::vector<int> left_drivetrain_motor_ports,
                  int left_rotation_tracking_wheel_ports,
                  int right_rotation_tracking_wheel_ports,
                  int center_rotation_tracking_wheel_ports,
-                 double tracker_gear_ratio, double tracker_wheel_diameter) {}
-void Chassis::initialize() {}
-void Chassis::set_defaults() {}
-void Chassis::set_tank_control() {}
-void Chassis::set_arcade_standard_control() {}
-void Chassis::set_arcade_flipped_control() {}
-void Chassis::set_joystick_threshold(int threshold) {}
-void Chassis::set_chassis_brake_mode(pros::motor_brake_mode_e_t brake_mode) {}
-void Chassis::reset_drive_sensors() {}
-void Chassis::reset_inertial_sensor(int heading) {}
-double Chassis::get_inertial_sensor_heading() {
-  return inertial_sensor->get_heading();
-}
-bool Chassis::is_inertial_sensor_calibrating() {
-  inertial_sensor->reset();
-  int time = 0;
-  while (true) {
-    time += util::delay_time;
-
-    if (time >= 2000) {
-      if (!(inertial_sensor->get_status() &
-            pros::c::E_IMU_STATUS_CALIBRATING)) {
-        break;
-      }
-      if (time >= 3000) {
-        printf("No IMU plugged in, (took %d ms to realize that)\n", time);
-        return false;
-      }
-    }
-    pros::delay(util::delay_time);
+                 double tracking_wheel_gear_ratio,
+                 double tracking_wheel_diameter)
+    : inertial_sensor(inertial_sensor_port),
+      left_encoder_tracking_wheel(-1, -1, -1),
+      right_encoder_tracking_wheel(-1, -1, -1),
+      center_encoder_tracking_wheel(-1, -1, -1),
+      left_rotation_tracking_wheel(
+          left_rotation_tracking_wheel_ports,
+          util::is_negative(left_rotation_tracking_wheel_ports)),
+      right_rotation_tracking_wheel(
+          right_rotation_tracking_wheel_ports,
+          util::is_negative(right_rotation_tracking_wheel_ports)),
+      center_rotation_tracking_wheel(
+          center_rotation_tracking_wheel_ports,
+          util::is_negative(center_rotation_tracking_wheel_ports)) {
+  for (auto i : left_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    left_tank_drive_motors.push_back(temp);
   }
-  master.rumble(".");
-  printf("IMU is done calibrating (took %d ms)\n", time);
-  return true;
-}
-int Chassis::left_sensor() {
-  if (tracker_type == 2) {
-    return left_encoder_tracking_wheel->get_value();
-  } else if (tracker_type == 3) {
-    return left_rotation_tracking_wheel->get_position();
+  for (auto i : right_drivetrain_motor_ports) {
+    pros::Motor temp = pros::Motor(abs(i), util::is_negative(i));
+    right_tank_drive_motors.push_back(temp);
   }
-  return left_tank_drive_motors.front().get_position();
+  tracker_type = tracker_rotation_wheel;
+  chassis_type = tank_drive;
+  drivetrain_cartidge_rpms = chassis_cartidge_rpms;
+  drivetrain_gear_ratio = chassis_gear_ratio;
+  drivetrain_wheel_diameter = chassis_wheel_diameter;
+  drivetrain_wheel_circumference = drivetrain_wheel_diameter * M_PI;
+  tracker_gear_ratio = tracking_wheel_gear_ratio;
+  tracker_wheel_diameter = tracking_wheel_diameter;
+  tracker_wheel_circumference = tracking_wheel_diameter * M_PI;
+  calculate_tracker_variables();
 }
-int Chassis::left_velocity() {
-  return left_tank_drive_motors.front().get_actual_velocity();
-}
-double Chassis::left_mA() {
-  return left_tank_drive_motors.front().get_current_draw();
-}
-bool Chassis::is_left_drive_over_current() {
-  return left_tank_drive_motors.front().is_over_current();
-}
-int Chassis::right_sensor() {
-  if (tracker_type == 2) {
-    return right_encoder_tracking_wheel->get_value();
-  } else if (tracker_type == 3) {
-    return right_rotation_tracking_wheel->get_position();
-  }
-  return right_tank_drive_motors.front().get_position();
-}
-int Chassis::right_velocity() {
-  return right_tank_drive_motors.front().get_actual_velocity();
-}
-double Chassis::right_mA() {
-  return right_tank_drive_motors.front().get_current_draw();
-}
-bool Chassis::is_right_drive_over_current() {
-  return right_tank_drive_motors.front().is_over_current();
-}
-double Chassis::calculate_wheel_circumference(int diameter) {
-  return diameter * M_PI;
-}
-double Chassis::calculate_tracker_tick_per_revoution() {
+void Chassis::calculate_tracker_variables() {
   if (tracker_type == 2 || tracker_type == 3) {
-    return tracker_tick_per_revolution =
-               drivetrain_cartidge_rpms * tracker_gear_ratio;
-  } else {
-    return tracker_tick_per_revolution =
-               ((50.0 * (3600.0 / drivetrain_cartidge_rpms)) *
-                drivetrain_gear_ratio);
+    tracker_tick_per_revolution = drivetrain_cartidge_rpms * tracker_gear_ratio;
+  } else if (tracker_type == 1) {
+    tracker_tick_per_revolution =
+        ((50.0 * (3600.0 / drivetrain_cartidge_rpms)) * tracker_gear_ratio);
   }
-}
-double Chassis::calculate_tracker_tick_per_inch() {
-  return tracker_tick_per_inch =
-             tracker_tick_per_revolution / tracker_wheel_circumference;
+  tracker_tick_per_revolution =
+      (tracker_tick_per_inch / tracker_wheel_circumference);
 }
 }; // namespace apollo
