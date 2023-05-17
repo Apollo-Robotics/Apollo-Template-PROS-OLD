b6e6fad033b583c43398f70f78764e6ac51c7492
nikgajjar51 <nikgajjar51@gmail.com>
2023-05-17 17:42:53 -0400

    Refactoring Okapi

--- include/main.h
+++ include/main.h
@@ -35,25 +35,23 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
-#include "Apollo-Lib/api.hpp"
 
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
-//#include "pros/api_legacy.h"
+// #include "pros/api_legacy.h"
+#include "Apollo-Lib/api.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
  * Motor, you can use the namespace with the following commented out line.
  *
- * IMPORTANT: Only the okapi or pros namespace may be used, not both
- * concurrently! The okapi namespace will export all symbols inside the pros
+ * IMPORTANT: Only the apollo or pros namespace may be used, not both
+ * concurrently! The apollo namespace will export all symbols inside the pros
  * namespace.
  */
 // using namespace pros;
 // using namespace pros::literals;
-// using namespace okapi;
 
 /**
  * Prototypes for the competition control tasks are redefined here to ensure
@@ -61,13 +59,14 @@
  * button press in opcontrol() for testing purposes).
  */
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
-void autonomous(void);
-void initialize(void);
-void disabled(void);
-void competition_initialize(void);
-void opcontrol(void);
+    void autonomous(void);
+    void initialize(void);
+    void disabled(void);
+    void competition_initialize(void);
+    void opcontrol(void);
 #ifdef __cplusplus
 }
 #endif
@@ -76,7 +75,7 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
-//#include <iostream>
+// #include <iostream>
 #endif
 
-#endif  // _PROS_MAIN_H_
+#endif // _PROS_MAIN_H_
